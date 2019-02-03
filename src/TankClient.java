dbf90959b29ae0ad6bedfea74565f1592ae6dd19
rqsir <hbrq1314@gmail.com>
2019-02-04 00:06:26 +0800

    version0.2 add window close function add window size unchangeable

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
 
@@ -6,6 +8,17 @@ public class TankClient extends Frame {
         this.setLocation(400,300);
         this.setSize(800,600);
         this.setVisible(true);
+
+        //add close window function
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //add window size unchangeable
+        this.setResizable(false);
     }
 
     public static void main(String[] args){
